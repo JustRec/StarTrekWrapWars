69a4040816c871debb2a2d8e8322feb964b8ff8d
JustRec <mmmollaoglumehmet@gmail.com>
2022-03-25 10:55:26 +0300

    beautify and re-design Moment.java

--- Moment.java
+++ Moment.java
@@ -1,114 +1,97 @@
 import enigma.core.Enigma;
 import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 
 public class Moment {
-   public enigma.console.Console cn = Enigma.getConsole("");
-   public KeyListener klis; 
+    public enigma.console.Console cn = Enigma.getConsole("");
+    public EnigmaWrapper wrapper;
 
-   // ------ Standard variables for mouse and keyboard ------
-   public int keypr;   // key pressed?
-   public int rkey;    // key   (for press/release)
-   // ----------------------------------------------------
-   public char[][] map;
-   public boolean energy2x;
-   
-   Moment(char[][] map, boolean energy2x) throws Exception {   // --- Contructor
-      this.map=map;
-      this.energy2x=energy2x;
-      klis=new KeyListener() {
-         public void keyTyped(KeyEvent e) {}
-         public void keyPressed(KeyEvent e) {
-            if(keypr==0) {
-               keypr=1;
-               rkey=e.getKeyCode();
-            }
-         }
-         public void keyReleased(KeyEvent e) {}
-      };
-      cn.getTextWindow().addKeyListener(klis);
-      // ----------------------------------------------------
-      
+    public char[][] map;
+    public boolean energy2x;
 
-      while(true) {
-         
-         if(keypr==1) {    // if keyboard button pressed
-            if(rkey==KeyEvent.VK_LEFT && Player.findPx(map) > 0 && map[Player.findPy(map)][Player.findPx(map) - 1] != '#') {
-                int cursorx = cn.getTextWindow().getCursorX();
-                int cursory = cn.getTextWindow().getCursorY();
-                
-                map[cursory][cursorx] = ' ';
-                map[cursory][cursorx - 1] = 'P';
+    Moment(char[][] map, boolean energy2x, EnigmaWrapper wrapper) throws Exception { // --- Contructor
+        this.map = map;
+        this.energy2x = energy2x;
+        this.wrapper = wrapper;
 
-                System.out.println(" "); //delete P
+        while (true) {
 
-                cn.getTextWindow().setCursorPosition(cursorx - 1, cursory);
-                System.out.println("P");
-                cn.getTextWindow().setCursorPosition(cursorx - 1, cursory);
+            if (wrapper.getKeypr() == 1) { // if keyboard button pressed
+                if (wrapper.getRkey() == KeyEvent.VK_LEFT && Player.findPx(map) > 0 && map[Player.findPy(map)][Player.findPx(map) - 1] != '#') {
+                    int cursorx = cn.getTextWindow().getCursorX();
+                    int cursory = cn.getTextWindow().getCursorY();
 
-            }
-            if(rkey==KeyEvent.VK_RIGHT && Player.findPx(map) < map[0].length - 1 && map[Player.findPy(map)][Player.findPx(map) + 1] != '#') {
-                int cursorx = cn.getTextWindow().getCursorX();
-                int cursory = cn.getTextWindow().getCursorY();
-                
-                map[cursory][cursorx] = ' ';
-                map[cursory][cursorx + 1] = 'P';
-                
-                System.out.println(" "); //delete P
+                    map[cursory][cursorx] = ' ';
+                    map[cursory][cursorx - 1] = 'P';
 
-                cn.getTextWindow().setCursorPosition(cursorx + 1, cursory);
-                System.out.println("P");
-                cn.getTextWindow().setCursorPosition(cursorx + 1, cursory);
-            }
-            if(rkey==KeyEvent.VK_UP && Player.findPy(map) > 0 && map[Player.findPy(map) - 1][Player.findPx(map)] != '#') {
-                int cursorx = cn.getTextWindow().getCursorX();
-                int cursory = cn.getTextWindow().getCursorY();
-                
-                map[cursory][cursorx] = ' ';
-                map[cursory - 1][cursorx] = 'P';
+                    System.out.println(" "); //delete P
 
-                System.out.println(" "); //delete P
+                    cn.getTextWindow().setCursorPosition(cursorx - 1, cursory);
+                    System.out.println("P");
+                    cn.getTextWindow().setCursorPosition(cursorx - 1, cursory);
 
-                cn.getTextWindow().setCursorPosition(cursorx, cursory - 1);
-                System.out.println("P");
-                cn.getTextWindow().setCursorPosition(cursorx, cursory - 1);
-            }
-            if(rkey==KeyEvent.VK_DOWN && Player.findPy(map) < map.length - 1 && map[Player.findPy(map) + 1][Player.findPx(map)] != '#') {
-                int cursorx = cn.getTextWindow().getCursorX();
-                int cursory = cn.getTextWindow().getCursorY();
-                
-                map[cursory][cursorx] = ' ';
-                map[cursory + 1][cursorx] = 'P';
-                
-                System.out.println(" "); //delete P
+                }
+                if (wrapper.getRkey() == KeyEvent.VK_RIGHT && Player.findPx(map) < map[0].length - 1 && map[Player.findPy(map)][Player.findPx(map) + 1] != '#') {
+                    int cursorx = cn.getTextWindow().getCursorX();
+                    int cursory = cn.getTextWindow().getCursorY();
 
-                cn.getTextWindow().setCursorPosition(cursorx, cursory + 1);
-                System.out.println("P");
-                cn.getTextWindow().setCursorPosition(cursorx, cursory + 1);
-            }
-            if(rkey==KeyEvent.VK_W) {
-               
-            }
-            if(rkey==KeyEvent.VK_S) {
-               
-            }
-            if(rkey==KeyEvent.VK_A) {
-               
-            }
-            if(rkey==KeyEvent.VK_D) {
-               
-            }
+                    map[cursory][cursorx] = ' ';
+                    map[cursory][cursorx + 1] = 'P';
 
-            if(rkey==KeyEvent.VK_SPACE) {
-               break;
+                    System.out.println(" "); //delete P
+
+                    cn.getTextWindow().setCursorPosition(cursorx + 1, cursory);
+                    System.out.println("P");
+                    cn.getTextWindow().setCursorPosition(cursorx + 1, cursory);
+                }
+                if (wrapper.getRkey() == KeyEvent.VK_UP && Player.findPy(map) > 0 && map[Player.findPy(map) - 1][Player.findPx(map)] != '#') {
+                    int cursorx = cn.getTextWindow().getCursorX();
+                    int cursory = cn.getTextWindow().getCursorY();
+
+                    map[cursory][cursorx] = ' ';
+                    map[cursory - 1][cursorx] = 'P';
+
+                    System.out.println(" "); //delete P
+
+                    cn.getTextWindow().setCursorPosition(cursorx, cursory - 1);
+                    System.out.println("P");
+                    cn.getTextWindow().setCursorPosition(cursorx, cursory - 1);
+                }
+                if (wrapper.getRkey() == KeyEvent.VK_DOWN && Player.findPy(map) < map.length - 1 && map[Player.findPy(map) + 1][Player.findPx(map)] != '#') {
+                    int cursorx = cn.getTextWindow().getCursorX();
+                    int cursory = cn.getTextWindow().getCursorY();
+
+                    map[cursory][cursorx] = ' ';
+                    map[cursory + 1][cursorx] = 'P';
+
+                    System.out.println(" "); //delete P
+
+                    cn.getTextWindow().setCursorPosition(cursorx, cursory + 1);
+                    System.out.println("P");
+                    cn.getTextWindow().setCursorPosition(cursorx, cursory + 1);
+                }
+                if (wrapper.getRkey() == KeyEvent.VK_W) {
+
+                }
+                if (wrapper.getRkey() == KeyEvent.VK_S) {
+
+                }
+                if (wrapper.getRkey() == KeyEvent.VK_A) {
+
+                }
+                if (wrapper.getRkey() == KeyEvent.VK_D) {
+
+                }
+
+                if (wrapper.getRkey() == KeyEvent.VK_SPACE) {
+                    break;
+                }
+
+                wrapper.setKeypr(0); // last action  
             }
-            
-            keypr=0;    // last action  
-         }
-         if(energy2x)
-            Thread.sleep(250);
-         else 
-            Thread.sleep(500);
-      }
-   }
+            if (energy2x)
+                Thread.sleep(250);
+            else
+                Thread.sleep(500);
+        }
+    }
 }
