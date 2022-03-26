fba6c1682a3e14c68807eaeb06296ee2eca5bb0c
2brk <102043811+2brk@users.noreply.github.com>
2022-03-26 13:09:44 +0000

    Update and rename InputQueue.java to ItemQueue.java

--- ItemQueue.java
+++ ItemQueue.java
@@ -1,13 +1,13 @@
 import java.util.Random;
 
-public class InputQueue {
+public class ItemQueue {
 	private static Random rand = new Random();
 	private static boolean firstrun = true;
+	static Queue iq = new Queue(32000);
 	private static int a;
-	public static char[] queue = new char[15];
-	private static char to_be_returned;
+	
 	private static char randItem() {
-			a = rand.nextInt(40);
+		    a = rand.nextInt(40);
 		if(a == 39) {return '*';}
 		if (a > 36) {return '=';}
 		if (a > 34) {return 'C';}
@@ -15,29 +15,17 @@ public class InputQueue {
 		if (a > 25) {return '4';}
 		if (a > 19) {return '3';}
 		if (a > 11) {return '2';}
-		else		{return '1';}
+		else	    {return '1';}
 	}
+	
 	static char get() {
-		if(firstrun) {
-			for(int x = 0; x < 15; x++) {
-				queue[x] = randItem();
-			}
-		firstrun = false;
-		}
-		to_be_returned = queue[0];
-		for(int x = 0; x < 14; x++) {
-			queue[x] = queue[x+1];
-		}
-		queue[14] = randItem();
-		return to_be_returned;
+		if(firstrun) { for(int x = 0; x < 15; x++) {iq.enqueue(randItem());} firstrun = false;} 
+		iq.enqueue(randItem());
+		return (char)iq.dequeue();
 	}
 	
 	static void init() { //tercihe bağlı, InputQueue'nun oyun başlamadan önce oluşturulabilmesi için metod
-		for(int x = 0; x < 15; x++) {
-			queue[x] = randItem();
-		}
-	firstrun = false;
+		for(int x = 0; x < 15; x++) {iq.enqueue(randItem());}
+		firstrun = false;
 	}
-	
-	
 }
