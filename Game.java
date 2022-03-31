762414a61eb9648a091db4ae298bceed49c01c31
2brk <102043811+2brk@users.noreply.github.com>
2022-03-31 12:57:15 +0000

    Update Game.java

--- Game.java
+++ Game.java
@@ -10,12 +10,13 @@ public class Game {
 	private static int keyPattern[] = new int[10]; // 37 -> left, 38 -> up, 39 -> right, 40 -> down, 87 -> W, 65 -> A, 83 -> S, 68 -> D
 	private static double prevTime = System.currentTimeMillis();
 	private static short pr = 0;
+	public static int Score = 0;
 	
 	static void start(char[][] mp, boolean energy2x, EnigmaWrapper wr) throws Exception {
 		wrapper = wr; map = mp; boolean eUsed = false; float timecount = 0;
 		Player player = new Player(wrapper);
-		
-        while(true) {
+
+        while(true) { //-- Game loop --
     		Thread.sleep(50);
 			
         	if(wrapper.getKeypr() == 1) {
@@ -51,7 +52,7 @@ public class Game {
 	private static int decPattern(int[] pat) {
 		int a = 9;
 		boolean endsEmpty = false; //pattern ends with empty
-		while(pat[a] == 0) { //remove unassigned numbers from pattern
+		while(pat[a] == 0) { //remove unassigned numbers
 			a--;
 		}
 		if(pat[a] == 1) {endsEmpty = true;}
@@ -68,79 +69,13 @@ public class Game {
 		
     }
 	
-	private static void PlayerAction(){
-		
-		
-	            if (action == 37 && Player.findPx(map) > 0
-	                && map[Player.findPy(map)][Player.findPx(map) - 1] == ' ') {
-	                int cursorx = cn.getTextWindow().getCursorX();
-	                int cursory = cn.getTextWindow().getCursorY();
+	private static void InitPlayerAction(){
 
-	                map[cursory][cursorx] = ' ';
-	                map[cursory][cursorx - 1] = 'P';
-
-	                System.out.println(" "); // delete P
-
-	                cn.getTextWindow().setCursorPosition(cursorx - 1, cursory);
-	                wrapper.printInColor(Color.orange, Color.cyan, "P");
-	                cn.getTextWindow().setCursorPosition(cursorx - 1, cursory);
-
-	            }
-	            if (action == 39 && Player.findPx(map) < map[0].length - 1
-	                && map[Player.findPy(map)][Player.findPx(map) + 1] == ' ') {
-	                int cursorx = cn.getTextWindow().getCursorX();
-	                int cursory = cn.getTextWindow().getCursorY();
-
-	                map[cursory][cursorx] = ' ';
-	                map[cursory][cursorx + 1] = 'P';
-
-	                System.out.println(" "); // delete P
-
-	                cn.getTextWindow().setCursorPosition(cursorx + 1, cursory);
-	                wrapper.printInColor(Color.orange, Color.cyan, "P");
-	                cn.getTextWindow().setCursorPosition(cursorx + 1, cursory);
-	            }
-	            if (action == 38 && Player.findPy(map) > 0
-	                && map[Player.findPy(map) - 1][Player.findPx(map)] == ' ') {
-	                int cursorx = cn.getTextWindow().getCursorX();
-	                int cursory = cn.getTextWindow().getCursorY();
-
-	                map[cursory][cursorx] = ' ';
-	                map[cursory - 1][cursorx] = 'P';
-
-	                System.out.println(" "); // delete P
-
-	                cn.getTextWindow().setCursorPosition(cursorx, cursory - 1);
-	                wrapper.printInColor(Color.orange, Color.cyan, "P");
-	                cn.getTextWindow().setCursorPosition(cursorx, cursory - 1);
-	            }
-	            if (action == 40 && Player.findPy(map) < map.length - 1
-	                && map[Player.findPy(map) + 1][Player.findPx(map)] == ' ') {
-	                int cursorx = cn.getTextWindow().getCursorX();
-	                int cursory = cn.getTextWindow().getCursorY();
-
-	                map[cursory][cursorx] = ' ';
-	                map[cursory + 1][cursorx] = 'P';
-
-	                System.out.println(" "); // delete P
-
-	                cn.getTextWindow().setCursorPosition(cursorx, cursory + 1);
-	                wrapper.printInColor(Color.orange, Color.cyan, "P");
-	                cn.getTextWindow().setCursorPosition(cursorx, cursory + 1);
-	            }
-	            if (action == 87) {
-
-	            }
-	            if (action == 83) {
-
-	            }
-	            if (action == 65) {
-
-	            }
-	            if (action == 68) {
-
-	            }	
-		
+		if (action == 37 && Player.findPx(map) > 0 && retCol(-1,0) != '#') {PlayerAction(-1,0);}
+		if (action == 39 && Player.findPx(map) < map[0].length - 1 && retCol(1,0) != '#') {PlayerAction(1,0);}
+		if (action == 38 && Player.findPy(map) > 0 && retCol(0,-1) != '#') {PlayerAction(0,-1);}
+		if (action == 40 && Player.findPy(map) < map.length - 1 && retCol(0,1) != '#') {PlayerAction(0,1);}
+	
 	}
 	
 	private static void Player() {
@@ -148,7 +83,7 @@ public class Game {
 		prevAction = action;      		
 		pr = 0;
 		keyPattern = new int[10];
-		PlayerAction();   		
+		InitPlayerAction();   		
 		prevTime = System.currentTimeMillis();
 	}
 	
@@ -157,5 +92,73 @@ public class Game {
 		//Bot hareketleri
 	}
 	
+	private static char retCol(int x, int y) { //return collision on relative locations
+		return map[Player.findPy(map) + y][Player.findPx(map) + x];
+	}
 	
+	private static void Draw(int x, int y, char c) {
+		
+		int cursorx = cn.getTextWindow().getCursorX();
+        int cursory = cn.getTextWindow().getCursorY();
+        
+        map[cursory][cursorx] = ' ';
+        map[cursory + y][cursorx + x] = c;
+        System.out.println(" ");
+        cn.getTextWindow().setCursorPosition(cursorx + x, cursory + y);
+        wrapper.printInColor(Color.orange, Color.cyan, Character.toString(c));
+        cn.getTextWindow().setCursorPosition(cursorx + x, cursory + y);
+	}
+	
+	private static void PlayerAction(int x, int y) {	
+		char col = retCol(x,y);
+		Draw(x,y,'P');
+
+		switch(col) {
+		case 'C':
+			endGame();
+			break;
+		case '1':
+			Score += 1;
+			printScore();
+			break;
+		case '2':
+			Score += 5;
+			Backpack.takeItem('2');
+			printScore();
+			break;
+		case '3':
+			Score += 15;
+			Backpack.takeItem('3');
+			printScore();
+			break;
+		case '4':
+			Score += 50;
+			Backpack.takeItem('4');
+			printScore();
+			break;
+		case '5':
+			Score += 150;
+			Backpack.takeItem('5');
+			printScore();
+			break;
+		case '=':
+			Backpack.takeItem('=');
+			break;
+		case '*':
+			Backpack.takeItem('*');
+			break;
+		}
+		
+	}
+	
+	private static void endGame() {
+		//
+	}
+	
+	private static void printScore() {
+		int x = cn.getTextWindow().getCursorX(), y = cn.getTextWindow().getCursorY();
+		cn.getTextWindow().setCursorPosition(57, 15);
+		System.out.printf("Score: %s", Score);
+		cn.getTextWindow().setCursorPosition(x, y);
+	}
 }
